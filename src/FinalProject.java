09ca96deeb6674ebb60b5d895bec5dfef1f9203f
Peter <Peter@Katrina>
2016-01-21 21:43:08 -0500

    Background, player, move keys added

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -1,8 +1,18 @@
-
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -10,18 +20,65 @@ import javax.swing.JFrame;
  */
 
 
-public class FinalProject extends JComponent{
+public class FinalProject extends JComponent implements KeyListener, MouseMotionListener, MouseListener{
 
     // Height and Width of our game
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
+    static final int WIDTH = 1350;
+    static final int HEIGHT = 700;
     
     // sets the framerate and delay for our game
     // you just need to select an approproate framerate
     long desiredFPS = 60;
     long desiredTime = (1000)/desiredFPS;
+   
+    //player 
+    Rectangle player;
     
+    // player position variables
+    int x = 100;
+    int y = 100;
+   
+    //platforms
+    ArrayList<Rectangle> platforms = new ArrayList<>();
+   
+    //platform width
+    int w = 75;
+    //platform random height between 100 and 300
+    int h = 1;
+   
+    //create gravity for jumping
+    int gravity = 1;
+    int frameCount = 0;
+   
+    //move
+    int xmove = 0;
+   
+    //keyboard variables
+    boolean right = false;
+    boolean left = false;
+    boolean jump = false;
+   
+    //move variables
+    int moveX = 0;
+    int moveY = 0;
+    
+    //import images
+    BufferedImage background = loadImage("background1.png");
+    BufferedImage standing = loadImage("playerStanding.jpg");
+    BufferedImage running = loadImage("playerRunning.gif");
 
+
+    
+    public BufferedImage loadImage(String filename){
+        BufferedImage img = null;
+        try{
+            img = ImageIO.read(new File(filename));
+        }catch(Exception e){
+            System.out.println("Error loading " + filename);
+        }
+        return img;
+    }
+    
     
     // drawing of the game happens in here
     // we use the Graphics object, g, to perform the drawing
@@ -33,7 +90,20 @@ public class FinalProject extends JComponent{
         g.clearRect(0, 0, WIDTH, HEIGHT);
         
         // GAME DRAWING GOES HERE 
+        // GAME DRAWING STARTS HERE
+        Color greyish = new Color(149, 190, 204);
+        g.setColor(greyish);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        g.drawImage(background, 0, 0, WIDTH, HEIGHT, null);
+       
+        //player
+        g.drawImage(standing, player.x, player.y, player.width, player.height, null);
         
+        //platforms
+        g.setColor(Color.BLACK);
+        //array of blocks
+        //MATH.random for heights?
+
         
         // GAME DRAWING ENDS HERE
     }
@@ -58,7 +128,14 @@ public class FinalProject extends JComponent{
             
             // all your game rules and move is done in here
             // GAME LOGIC STARTS HERE 
-            
+            //move the player
+             if(right){
+                xmove = 1;
+            } else if(left){
+                xmove = -1;
+            }else {
+                xmove = 0;
+            }
             
 
             // GAME LOGIC ENDS HERE 
@@ -107,5 +184,63 @@ public class FinalProject extends JComponent{
         
         // starts my game loop
         game.run();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        //when the arrow keys & space bar are pressed
+        int key = ke.getKeyCode();
+        if(key == KeyEvent.VK_SPACE){
+            jump = true;
+        }else if(key == KeyEvent.VK_LEFT){
+            left = true;
+        }else if(key == KeyEvent.VK_RIGHT){
+            right = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+         //when the arrow keys & space bar are released
+        int key = ke.getKeyCode();
+        if(key == KeyEvent.VK_SPACE){
+            jump = false;
+        }else if(key == KeyEvent.VK_LEFT){
+            left = false;
+        }else if(key == KeyEvent.VK_RIGHT){
+            right = false;
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
     }
 }
